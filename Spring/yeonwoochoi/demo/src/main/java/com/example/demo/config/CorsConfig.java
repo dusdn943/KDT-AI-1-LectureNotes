cb84cebfacc3943c888896deda6f554f16667efa
dusdn943 <dusdn943@gmail.com>
2023-06-21 10:55:30 +0900

    feat: [최연우] 게시판 리스트 만들기 [REACT-BOARDx]

--- Spring/yeonwoochoi/demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ Spring/yeonwoochoi/demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://localhost:8080",
                         "http://192.168.20.3:8080",
                         "http://localhost:3000",
-                        "http://localhost:3001",
+                        "http://localhost:3001", // vue 게시판
+                        "http://localhost:3004", // react로 만든 게시판
                         "http://localhost:3010"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
