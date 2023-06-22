d723746eb410b6803867c6859f31627e3ae42d35
dusdn943 <dusdn943@gmail.com>
2023-06-22 16:06:01 +0900

    update: [최연우] Zustand 사용 형태 주석 추가 [ZUSTAND-TODOx]

--- mfe_react_vue/yeonwoo/mfe/react-zustand-todo-app/src/components/ZustandTodoList.jsx
+++ mfe_react_vue/yeonwoo/mfe/react-zustand-todo-app/src/components/ZustandTodoList.jsx
@@ -3,6 +3,8 @@ import ZustandTodoItem from './ZustandTodoItem'
 import { useTodoStore } from '../zustand/store'
 
 const ZustandTodoList = () => {
+    // Vue의 ...state()와 동일
+    // 리액트 용도가 명확하지 않고 두루뭉실하게 들어감
   const todos = useTodoStore((state) => state.todos)
 
   return (
